1e49e2780f575bc54a6ed159839cfdab4e21eac1
Bernard <bsuwirjo@gmail.com>
2020-02-27 17:14:09 -0800

    Finished lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,26 +2,51 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-  initializePage();
+ initializePage();
 })
 
 /*
- * Function that is called when the document is ready.
- */
+* Function that is called when the document is ready.
+*/
 function initializePage() {
-	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
- 	initGestures();
- 	initRSVPForm();
+   console.log("Page ready");
+   initCamera();
+   initMap();
+   initGestures();
+   initRSVPForm();
 }
 
-// init jQuery gestures  
+// init jQuery gestures
 function initGestures() {
-	// add gestures listener here
+   // add gestures listener here
+   $(function(){
+       $(".judge-img").bind("taphold", tapholdHandler);
+
+       function tapholdHandler(event) {
+           var targetIDPrefix = event.target.id;
+           $("#" + targetIDPrefix + "-bio").show()
+       }
+   });
 }
+
+$(function() {
+   $("div.box").bind("taphold", tapholdHandler);
+   function tapholdHandler(event) {
+       $(event.target).addClass("taphold");
+   }
+});
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+ // add your code here
+ $('#rsvpForm').submit(function(e) {
+   e.preventDefault();
+   var rsvpEmail = $('#rsvpEmail').val();
+   $.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+ });
+
+ function postCallback(res) {
+   alert("RSVP form successfully submitted!");
+   $('#rsvpEmail').val('');
+ }
 }
